570884b07a94ab4b4074e6896888240151ce7a5f
JuliusvonSmercek <juliusvonsmercek@protonmail.com>
2023-01-02 14:31:27 +0100

    split templates

---
+++ templateCpp/a.cpp
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+long long solve(vector<string> &lines) {
+  // for (string &line : lines) {
+  //   stringstream ss(line);
+  // }
+  // vector<vector<char>> data = readMatrix(lines);
+  // vector<vector<int>> data = readInput<int>(lines);
+  long long result = 0;
+
+  
+
+  return result;
+}
